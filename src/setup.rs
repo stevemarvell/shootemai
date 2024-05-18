e184208293caa06c25b7c7d0db27fdba6c7b8332
Steve Marvell <stevemarvell@crucialtechnical.com>
2024-05-18 14:44:05 +0100

    housekeeping

--- src/setup.rs
+++ src/setup.rs
@@ -8,7 +8,7 @@ pub struct SetupPlugin;
 
 impl Plugin for SetupPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (setup_cameras, setup_lighting, setup_terrain ));
-        app.add_systems(Startup, (add_player ));
+        app.add_systems(Startup, (setup_cameras, setup_lighting, setup_terrain));
+        app.add_systems(Startup, add_player);
     }
 }
