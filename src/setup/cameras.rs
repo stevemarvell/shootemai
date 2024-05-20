934fce4a183421a241cb92f0f60579debb904c63
Steve Marvell <stevemarvell@crucialtechnical.com>
2024-05-20 13:44:42 +0100

    follow cam

--- src/setup/cameras.rs
+++ src/setup/cameras.rs
@@ -7,7 +7,7 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_camera);
-        app.add_systems(Update, follow_marker);
+        app.add_systems(Update, look_at_marker);
     }
 }
 #[derive(Component)]
@@ -24,7 +24,7 @@ pub fn spawn_camera(mut commands: Commands) {
 
 const CAMERA_SPEED: f32 = 1.0;
 
-pub fn follow_marker(
+pub fn look_at_marker(
     mut camera_query: Query<&mut Transform, With<Camera>>,
     follow_marker_query: Query<&Transform, (With<FollowMarker>, Without<Camera>)>,
 ) {
