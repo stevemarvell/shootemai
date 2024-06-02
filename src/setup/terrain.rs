ac25187b065b316a52aad043be31a62ff47473fe
Steve Marvell <stevemarvell@crucialtechnical.com>
2024-06-02 11:44:38 +0100

    grass

--- src/setup/terrain.rs
+++ src/setup/terrain.rs
@@ -9,12 +9,44 @@ pub fn setup_terrain(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
+    // Define RGBA colors for grass
+    let fresh_spring_grass = Color::Rgba {
+        red: 124.0 / 255.0,
+        green: 252.0 / 255.0,
+        blue: 0.0 / 255.0,
+        alpha: 1.0,
+    };
+    let healthy_summer_grass = Color::Rgba {
+        red: 34.0 / 255.0,
+        green: 139.0 / 255.0,
+        blue: 34.0 / 255.0,
+        alpha: 1.0,
+    };
+    let standard_lawn_grass = Color::Rgba {
+        red: 85.0 / 255.0,
+        green: 107.0 / 255.0,
+        blue: 47.0 / 255.0,
+        alpha: 1.0,
+    };
+    let dry_autumn_grass = Color::Rgba {
+        red: 189.0 / 255.0,
+        green: 183.0 / 255.0,
+        blue: 107.0 / 255.0,
+        alpha: 1.0,
+    };
+    let dark_lush_grass = Color::Rgba {
+        red: 0.0 / 255.0,
+        green: 100.0 / 255.0,
+        blue: 0.0 / 255.0,
+        alpha: 1.0,
+    };
+
     let floor = (
         Name::new("Floor"),
         PbrBundle {
             mesh: meshes.add(Plane3d::default().mesh().size(50.0, 50.0)),
             material: materials.add(StandardMaterial {
-                base_color: Color::GREEN,
+                base_color: healthy_summer_grass,
                 perceptual_roughness: 1.0,
                 ..default()
             }),
